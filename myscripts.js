345a58ab226d2794bbab8b2b02887821a185c89b
Akhil <akki.mylavarapu@gmail.com>
2023-10-13 17:44:51 +0530

    made a result checker

--- myscripts.js
+++ myscripts.js
@@ -4,10 +4,18 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function playerChoice(){
+function getplayerChoice(){
     let pChoice = prompt("Enter either 'Rock', 'Paper', or 'Scissors'").charAt(0).toLowerCase();
     while (pChoice != "r" && pChoice != "p" && pChoice != "s") {
         pChoice = prompt("Enter either 'Rock', 'Paper', or 'Scissors'").charAt(0).toLowerCase();
     }
     return pChoice;
 }
+
+function checkResult(pChoice, cChoice) {
+    if (pChoice == cChoice) return "It's a tie!";
+    else if ((pChoice == "r" && cChoice == "s") || (pChoice == "p" && cChoice == "r") || (pChoice == "s" && cChoice == "p")) return ("You win! " + pChoice + " beats " + cChoice);
+    else return ("You lose, " + cChoice + " beats " + pChoice);
+}
+
+console.log(checkResult(getplayerChoice(), getComputerChoice()));
