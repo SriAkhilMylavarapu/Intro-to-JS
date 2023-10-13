b5961a6f29ec2572afd00dbd7754e1dfbe93cdf0
Akhil <akki.mylavarapu@gmail.com>
2023-10-13 17:33:57 +0530

    added prompt for player choice

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,13 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["r", "p", "s"];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+function playerChoice(){
+    let pChoice = prompt("Enter either 'Rock', 'Paper', or 'Scissors'").charAt(0).toLowerCase();
+    while (pChoice != "r" && pChoice != "p" && pChoice != "s") {
+        pChoice = prompt("Enter either 'Rock', 'Paper', or 'Scissors'").charAt(0).toLowerCase();
+    }
+    return pChoice;
+}
